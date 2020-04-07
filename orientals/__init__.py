241358caf5d8127f1c20417e4ab6a03131b04f17
ammedmar <ammedmar@gmail.com>
2020-04-07 16:33:24 +0200

    Add Simplex with __lt__function
    Signed-off-by: ammedmar <ammedmar@gmail.com>

--- orientals/__init__.py
+++ orientals/__init__.py
@@ -1,60 +1,59 @@
 from clesto import Module_element
 
 
-class ADC_element(Module_element):
+class Simplex(tuple):
+    def __lt__(self, other):
+        '''...'''
+        def lt(x, y):
+            if x[0] < y[0]:
+                return True
+            if x[0] == y[0]:
+                return lt(x[1:], y[1:])
+
+        x = sorted(self)
+        y = sorted(other)
+        return lt(x, y)
+
+    def atom(self):
+        '''...'''
+        if isinstance(self, int):
+            self = tuple(range(self + 1))
+        answer = {'-': [SADC_element({self: 1})],
+                  '+': [SADC_element({self: 1})]}
+
+        for i in range(len(self) - 1):
+            minus, plus = answer['+'][i].split_boundary()
+            print(minus)
+            answer['-'].append(minus)
+            answer['+'].append(plus)
+
+        return Mu_element(tuple(answer['-'][::-1]),
+                          tuple(answer['+'][::-1]))
+
+
+class SADC_element(Module_element):
     ''' Augmented Directed Complex
     '''
 
+    def __lt__(self, other):
+        return all((v > 0 for v in (other - self).values()))
+
     def split(self):
         '''...'''
-        m = ADC_element({k: abs(v) for k, v in self.items() if v < 0})
-        p = ADC_element({k: abs(v) for k, v in self.items() if v > 0})
+        m = SADC_element({k: abs(v) for k, v in self.items() if v < 0})
+        p = SADC_element({k: abs(v) for k, v in self.items() if v > 0})
         return m, p
 
     def boundary(self):
-        boundary = ADC_element()
+        boundary = SADC_element()
         for spx, v in self.items():
             for i in range(len(spx)):
-                boundary += ADC_element(
+                boundary += SADC_element(
                     {spx[:i] + spx[i + 1:]: v * (-1)**(i % 2)})
-
         return boundary
 
     def split_boundary(self):
         return self.boundary().split()
-
-    @classmethod
-    def atom(self, simplex):
-        '''...'''
-        if isinstance(simplex, int):
-            simplex = tuple(range(simplex + 1))
-
-        answer = {'-': [ADC_element({simplex: 1})],
-                  '+': [ADC_element({simplex: 1})]}
-
-        for i in range(len(simplex) - 1):
-            minus, plus = answer['+'][i].split_boundary()
-            answer['-'].append(minus)
-            answer['+'].append(plus)
-
-        return Mu_element(tuple(answer['-'][::-1]),
-                          tuple(answer['+'][::-1]))
-
-    @classmethod
-    def atom(self, simplex):
-        '''...'''
-        if isinstance(simplex, int):
-            simplex = tuple(range(simplex + 1))
-        answer = {'-': [ADC_element({simplex: 1})],
-                  '+': [ADC_element({simplex: 1})]}
-
-        for i in range(len(simplex) - 1):
-            minus, plus = answer['+'][i].split_boundary()
-            answer['-'].append(minus)
-            answer['+'].append(plus)
-
-        return Mu_element(tuple(answer['-'][::-1]),
-                          tuple(answer['+'][::-1]))
 
 
 class Mu_element:
@@ -67,7 +66,7 @@ class Mu_element:
         self.plus = plus
 
     def __eq__(self, other):
-        if isinstance(other, ADC_element):
+        if isinstance(other, SADC_element):
             return self.__dict__ == other.__dict__
         return NotImplemented
 
@@ -98,12 +97,12 @@ class Mu_element:
             raise TypeError(f'corresponding target & source must be equal')
 
         p, q = len(self.minus), len(other['-'])
-        c = (self.target(k)['-'] + (ADC_element(),) * max(0, abs(q - p)),
-             self.target(k).plus) + (ADC_element(),) * max(0, abs(q - p))
-        a = ((self.minus + (ADC_element(),) * max(0, q - p)),
-             (self.plus + (ADC_element(),) * max(0, q - p)))
-        b = ((other['-'] + (ADC_element(),) * max(0, p - q)),
-             (other.plus + (ADC_element(),) * max(0, p - q)))
+        c = (self.target(k)['-'] + (SADC_element(),) * max(0, abs(q - p)),
+             self.target(k).plus) + (SADC_element(),) * max(0, abs(q - p))
+        a = ((self.minus + (SADC_element(),) * max(0, q - p)),
+             (self.plus + (SADC_element(),) * max(0, q - p)))
+        b = ((other['-'] + (SADC_element(),) * max(0, p - q)),
+             (other.plus + (SADC_element(),) * max(0, p - q)))
         return Mu_element(
             (x + y - z for x, y, z in zip(a[0], b[0], c[0])),
             (x + y - z for x, y, z in zip(a[1], b[1], c[1])))
