371370850e91246f9696e947484a2470e16b7c01
ammedmar <ammedmar@gmail.com>
2020-04-09 18:19:18 +0200

    Add decompose function
    Signed-off-by: ammedmar <ammedmar@gmail.com>

--- orientals/__init__.py
+++ orientals/__init__.py
@@ -2,17 +2,36 @@ from clesto import Module_element
 
 
 class Simplex(tuple):
+
+    @property
+    def dimension(self):
+        return len(self) - 1
+
+    def __str__(self):
+        '''...'''
+        return super().__str__(self).replace(', ', ',')
+
     def __lt__(self, other):
         '''...'''
         def lt(x, y):
             if x[0] < y[0]:
                 return True
             if x[0] == y[0]:
-                return lt(x[1:], y[1:])
+                return not lt(x[1:], y[1:])
 
         x = sorted(self)
         y = sorted(other)
         return lt(x, y)
+
+    def boundary(self):
+        bdry = {0: set(), 1: set()}
+        for i in range(len(self)):
+            bdry[(i % 2)].add(Simplex(self[:i] + self[i + 1:]))
+        return bdry
+
+    @classmethod
+    def standard(self, d):
+        return self(range(d + 1))
 
     def atom(self):
         '''...'''
@@ -23,9 +42,8 @@ class Simplex(tuple):
 
         for i in range(len(self) - 1):
             minus, plus = answer['+'][i].split_boundary()
-            print(minus)
-            answer['-'].append(minus)
-            answer['+'].append(plus)
+            answer['-'].append(minus.sort())
+            answer['+'].append(plus.sort())
 
         return Mu_element(tuple(answer['-'][::-1]),
                           tuple(answer['+'][::-1]))
@@ -33,10 +51,8 @@ class Simplex(tuple):
 
 class SADC_element(Module_element):
     ''' Augmented Directed Complex
+    Module_element of basis_elements
     '''
-
-    def __lt__(self, other):
-        return all((v > 0 for v in (other - self).values()))
 
     def split(self):
         '''...'''
@@ -45,21 +61,27 @@ class SADC_element(Module_element):
         return m, p
 
     def boundary(self):
-        boundary = SADC_element()
-        for spx, v in self.items():
-            for i in range(len(spx)):
-                boundary += SADC_element(
-                    {spx[:i] + spx[i + 1:]: v * (-1)**(i % 2)})
-        return boundary
+        bdry = SADC_element()
+        for belmt, v in self.items():
+            for i in range(2):
+                bdry += SADC_element(
+                    {s: ((-1) ** i) * v for s in belmt.boundary()[i]})
+        return bdry
 
     def split_boundary(self):
         return self.boundary().split()
+
+    def sort(self):
+        '''...'''
+        sorted_data = sorted(self.items(), key=lambda pair: pair[0])
+        return SADC_element(dict(sorted_data))
 
 
 class Mu_element:
     '''...'''
 
     def __init__(self, minus, plus):
+        '''two tuples of SADC_element'''
         # TODO: perform input check
 
         self.minus = minus
@@ -71,9 +93,11 @@ class Mu_element:
         return NotImplemented
 
     def __hash__(self):
+        '''...'''
         return hash(tuple(sorted(self.__dict__.items())))
 
     def __repr__(self):
+        '''...'''
         return f'Mu_element({self.minus}, {self.plus})'
 
     def __str__(self):
@@ -81,15 +105,18 @@ class Mu_element:
                           zip(self.minus, self.plus)))
         return string.replace(', ', ',')
 
+    @property
+    def dimension(self):
+        '''...'''
+        return len(self.minus) - 1
+
     def source(self, k):
         '''...'''
-        return Mu_element(self.minus[:k + 1],
-                          self.plus[:k] + (self.minus[k],))
+        return Mu_element(self.minus[:k + 1], self.plus[:k] + (self.minus[k],))
 
     def target(self, k):
         '''...'''
-        return Mu_element(self.minus[:k] + (self.plus[k],),
-                          self.plus[:k + 1])
+        return Mu_element(self.minus[:k] + (self.plus[k],), self.plus[:k + 1])
 
     def compose(self, other, k):
         '''...'''
@@ -106,3 +133,27 @@ class Mu_element:
         return Mu_element(
             (x + y - z for x, y, z in zip(a[0], b[0], c[0])),
             (x + y - z for x, y, z in zip(a[1], b[1], c[1])))
+
+    def decompose(self):
+        '''...'''
+        def decomp(bracket):
+            if min(s.dimension for s in bracket) == 1 or len(bracket) == 1:
+                return bracket
+            bracket = [[term] for term in bracket]
+            for i in range(len(bracket) - 1):
+                x, y = bracket[i], bracket[i + 1]
+                max_x = sorted(x, key=lambda x_i: x_i.dimension)[-1]
+                max_y = sorted(y, key=lambda y_i: y_i.dimension)[-1]
+                comp_deg = min(max_x.dimension, max_y.dimension) - 1
+                forward_x = (max_x.atom().plus[comp_deg] + SADC_element(
+                    {spx: 1 for spx in filter(lambda x_i: x_i != max_x, x)}))
+                backward_y = (max_y.atom().minus[comp_deg] + SADC_element(
+                    {spx: 1 for spx in filter(lambda y_i: y_i != max_y, y)}))
+                h = forward_x - backward_y
+                bracket[i] = sorted(x + [k for k, v in h.items() if v < 0])
+                bracket[i + 1] = sorted(y + [k for k, v in h.items() if v > 0])
+
+            return [decomp(subbracket) for subbracket in bracket]
+
+        bracket = list(self.minus[-1].keys())
+        return decomp(bracket)
