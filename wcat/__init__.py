e40b23036be58dd2b51dfbef757d48b0db8d6737
ammedmar <ammedmar@gmail.com>
2020-07-20 15:32:35 +0200

    Add integration with simplicial_operators
    Signed-off-by: ammedmar <ammedmar@gmail.com>

--- wcat/__init__.py
+++ wcat/__init__.py
@@ -195,7 +195,13 @@ class Simplex(tuple):
             if x[0] < y[0]:
                 return True
             if x[0] == y[0]:
-                return not lt(x[1:], y[1:])
+                try:
+                    return not lt(x[1:], y[1:])
+                except IndexError:
+                    if len(x) == 1:
+                        return True
+                    else:
+                        False
 
         x = sorted(self)
         y = sorted(other)
